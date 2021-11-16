14d4bd585783b1a6efeaffb3d063376b831c53b4
kagwathi <91314834+kagwathi@users.noreply.github.com>
2021-11-16 10:51:20 -0800

    Add files via upload

---
+++ mercy  LIBRARY.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+/*
+project:library system m
+author:mercy njeri
+date:oct 2021
+language:c99
+compiler:GCC
+license:MIT
+
+*/
+printf("hello!!! this is my library system project\n");
+printf("tcounty library system\n");
+printf("welcome mercy\n");
+printf("what would you like to do\n");
+printf("1.add a new user\n");
+printf("2.get a new book\n");
+printf("3.borrow a book\n");
+printf("4.access the internet\n");
+printf("5.write reports\n");
+printf("6.do research\n");
+printf("7.return a book\n");
+
+	
+	
+
+	
+	
+	
+	
+	return 0;
+	
+	
+}
